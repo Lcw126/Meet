48ee0f548b49b1a2187690af476fce2593bad137
lcw126 <lcw126@naver.com>
2019-11-01 13:53:50 +0900

    gender choice

--- app/src/main/java/com/lcw/meet/AccountActivity.java
+++ app/src/main/java/com/lcw/meet/AccountActivity.java
@@ -13,9 +13,17 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class AccountActivity extends AppCompatActivity {
     EditText et_gender, et_birthday;
-    private DatePickerDialog.OnDateSetListener callbackMethod;
+
+
+    AlertDialog alertDialog1;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +36,53 @@ public class AccountActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 Toast.makeText(AccountActivity.this, "성별을 선택하세요.", Toast.LENGTH_SHORT).show();
+                final CharSequence[] values = {"남성","여성"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(AccountActivity.this);
+
+                builder.setTitle("Select Your Choice");
+
+                builder.setSingleChoiceItems(values, -1, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int item) {
+
+                        switch(item)
+                        {
+                            case 0:
+
+                                Toast.makeText(AccountActivity.this, "First Item Clicked", Toast.LENGTH_LONG).show();
+                                et_gender.setText(values[item]);
+                                break;
+                            case 1:
+
+                                Toast.makeText(AccountActivity.this, "Second Item Clicked", Toast.LENGTH_LONG).show();
+                                et_gender.setText(values[item]);
+                                break;
+
+                        }
+                        alertDialog1.dismiss();
+                    }
+                });
+                alertDialog1 = builder.create();
+                alertDialog1.show();
 
 
                 return false;
             }
-        });
+        });// et_gender.setOnTouchListener ..
+
+        et_birthday.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+
+
+                return false;
+            }
+        });//  et_birthday.setOnTouchListener ..
 
 
 
     }//onCreate ..
-
 
 
 
